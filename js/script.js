9a60ccdaaea695480723212e5fc3de82b3456e57
vinnabaptist <vinna.baptist@ifminvestors.com>
2015-11-26 20:10:58 +1100

    Draft + Form Assignment
    Week 8 - draft and form assignment

--- js/script.js
+++ js/script.js
@@ -1,7 +1,37 @@
+// mobile nav //
 function toggleMobileNav(){
     $('nav, .mobile-nav').toggleClass('open');
   }
 
 $(document).ready(function(){
   $('.mobile-nav').click(toggleMobileNav);
+});
+
+// recipe slideToggle //
+
+// $('.recipe').click(function(){
+//         $('#show-on-click').slideDown();
+//         $('.recipe').hide();
+//         $('.close').show();
+//         event.preventDefault();
+//     });
+
+// $('.close').click(function(){
+//     $('#show-on-click').slideUp();
+//     $('.close').hide();
+//     $('.recipe').show();
+//     event.preventDefault();
+// });
+
+$(".recipe").click(function() {
+        $(this).prev().slideDown();
+        $(this).hide();
+        event.preventDefault();
+    });
+
+$(".close").click(function() {
+    $(this).parent().slideUp();
+    $(this).parent().next().show();
+    event.preventDefault();
+
 });
